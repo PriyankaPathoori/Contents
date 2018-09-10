6c092644f4369763fbc8d2b96bfdcdb3a912e69b
user2 user2 <user2@wavemaker.com>
2018-09-10 14:28:35 +0530

    Initial Push

---
+++ src/main/webapp/WEB-INF/prefabs/Dep1/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Dep1",
+  "homePage" : "Main",
+  "name" : "Dep1",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
